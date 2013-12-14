88d245f60363e9fe5ddfbea855d1ebba96a8231f
Paul <pebre79@hotmail.com>
2013-12-14 01:03:29 -0600

    Update settings.py

--- myproj/settings.py
+++ myproj/settings.py
@@ -26,6 +26,8 @@ DATABASES = {
     }
 }
 
+FIXTURE_DIRS = (DJANGO_ROOT,)
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
